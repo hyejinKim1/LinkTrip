0f761de035fe197bbf30f95b45f90cac4a7e9e15
Hgstudio <tndms0308@naver.com>
2023-11-19 21:43:53 +0900

    commit

--- src/components/user/MyPage.vue
+++ src/components/user/MyPage.vue
@@ -1,10 +1,21 @@
 <script setup>
+import { storeToRefs } from "pinia";
+import { useMemberStore } from "@/stores/member";
+
+const memberStore = useMemberStore();
+const { isLogin } = storeToRefs(memberStore);
+
+const logout = async () => {
+    console.log('logout');
+}
+
 
 </script>
 
 <template>
     <div>
         <h1>My Page</h1>
+        <button type="button" @click="logout">로그아웃</button>
     </div>
 </template>
 
