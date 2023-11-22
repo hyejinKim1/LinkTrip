03eb3772434a4544f91d8167d6d2a69113ea05b2
김혜진 <hyejin9203@naver.com>
2023-11-23 00:40:24 +0900

    화면 크기 수정

--- src/components/community/LikeArticle.vue
+++ src/components/community/LikeArticle.vue
@@ -13,12 +13,12 @@ onMounted(() => {
 })
 
 const getLikeArticle = () => {
-  let baseUrl = VITE_VUE_API_URL+"community/listLikedArticle?" + "userId=" + memberStore.userInfo.userId;
+  let baseUrl = VITE_VUE_API_URL + "community/listLikedArticle?" + "userId=" + memberStore.userInfo.userId;
   console.log(baseUrl);
 
   axios.get(baseUrl)
     .then((res) => {
-        console.log(res);
+      console.log(res);
       console.log(res.data.articleList);
       LikeArticle.value = res.data.articleList;
     });
@@ -26,11 +26,56 @@ const getLikeArticle = () => {
 </script>
 
 <template>
-    <div>
-        
+  <div id="container" class="container">
+    <div class="row">
+      <div class="article col-lg-3" v-for="article in LikeArticle" :key="article.articleIdx">
+        <h2 class="card-title">{{ article.articleTitle }}</h2>
+        <p class="card-text">{{ article.content }}</p>
+        <p class="date card-text">Created at: {{ article.createAt }}</p>
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
+/* #container{
+  margin-top:9vh;
 
+  width: 100vw;
+} */
+
+#container {
+  height: 100vh;
+  width: 100vw;
+  padding-top: 9vh;
+  overflow: hidden;
+}
+
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+}
+
+.article {
+  background-color: #fff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  border-radius: 8px;
+  width: 40vw;
+  box-sizing: border-box;
+}
+
+h2 {
+  color: #333;
+}
+
+p {
+  color: #666;
+}
+
+.date {
+  color: #999;
+  font-size: 14px;
+}
 </style>
