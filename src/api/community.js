85ee9f61051639e1474510771e36f6b54d53d091
fpssj <fpssj@naver.com>
2023-11-24 10:08:27 +0900

    createArticle 버그 수정

--- src/api/community.js
+++ src/api/community.js
@@ -1,16 +1,16 @@
-import { localAxios } from "@/util/http-commons";
+import {localAxios} from "@/util/http-commons";
 
 const local = localAxios();
 
 async function getArticleList(param) {
     console.log(param);
-    const result = await local.get(`community/listArticle`, { params: param });
+    const result = await local.get(`community/listArticle`, {params: param});
     return result.data;
 }
 
 async function deleteComment(param) {
     console.log(param);
-    await local.delete(`community/deleteComment`, { params: param });
+    await local.delete(`community/deleteComment`, {params: param});
 }
 
 async function createComment(param) {
@@ -30,18 +30,19 @@ async function createScrap(param) {
 
 async function checkLikeArticle(param) {
     console.log(param);
-    const result = await local.get(`community/checkLikeArticle`, { params: param })
+    const result = await local.get(`community/checkLikeArticle`, {params: param})
     return result.data.isLike;
 }
 
 async function createLikeArticle(param) {
     console.log(param);
-    await local.post(`community/createLikeArticle`, null, { params: param });
+    await local.post(`community/createLikeArticle`, null, {params: param});
 }
 
 async function deleteLikeArticle(param) {
-    await local.delete(`/community/deleteLikeArticle`, { params: param });
+    await local.delete(`/community/deleteLikeArticle`, {params: param});
 }
+
 async function updateArticle(param) {
     console.log(param);
     await local.patch(`community/updateArticle`, param);
@@ -49,22 +50,25 @@ async function updateArticle(param) {
 
 async function createArticle(param) {
     console.log(param);
-    await local.post(`community/createArticle`, paramzzz);
+    await local.post(`community/createArticle`, param, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }
+    );
 }
 
 async function listMyArticle(param) {
     console.log(param);
-    const result = await local.get(`community/listMyArticle`, { params: param });
+    const result = await local.get(`community/listMyArticle`, {params: param});
     return result.data;
 }
 
 async function listLikedArticle(param) {
     console.log(param);
-    const result = await local.get(`community/listLikedArticle`, { params: param });
+    const result = await local.get(`community/listLikedArticle`, {params: param});
     return result.data;
 }
-
-
 
 
 export {
